a15b804ae4b63014f2e6e9c22d0ccef8c1534800
RajatGanguly <rajatganguly01@gmail.com>
2023-08-21 13:51:34 +0530

    Modified markup and products are dynamically fetched from database

--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,61 @@
 from django.shortcuts import render
+from .models import Product, Contact
+from math import ceil
+
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+# Create your views here.
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return HttpResponse("We are at about")
+    return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
-def productView(request):
-    return HttpResponse("We are at product view")
+def productView(request, myid):
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+
+
+    return render(request, 'shop/prodView.html', {'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, 'shop/checkout.html')
